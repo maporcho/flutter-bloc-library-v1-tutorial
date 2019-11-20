67116e6d50526155b3832b2a629fc1e3880e07b5
Marco Antonio Porcho Souza <marco.porcho-souza@serpro.gov.br>
2019-11-20 17:35:39 -0300

    Initial page working.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_v1_tutorial/data/bloc.dart';
+import 'package:flutter_bloc_v1_tutorial/data/weather_repository.dart';
 
 import 'pages/weather_search_page.dart';
 
@@ -8,8 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Weather App',
-      home: WeatherSearchPage(),
-    );
+        title: 'Weather App',
+        home: BlocProvider(
+          builder: (context) => WeatherBloc(FakeWeatherRepository()),
+          child: WeatherSearchPage(),
+        ));
   }
 }
